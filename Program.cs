be88f3f252be0f32d5ef49998f228c2162f03cd3
elif buse alp <elifbusealp@gmail.com>
2022-08-02 15:54:17 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿// ----------------- array methodları --------------------------
+
+int[] dizi = { 23,89,54,6,7,1,13,45};
+
+//-------sırasız dizi---------
+
+foreach (var sayi in dizi)
+{
+    Console.WriteLine(sayi);
+}
+
+//-----sıralı dizi----------
+Console.WriteLine("------------------sıralı dizi-------------");
+
+
+Array.Sort(dizi);
+
+foreach (var sayi in dizi)
+{
+    Console.WriteLine(sayi);
+}
+
+Console.WriteLine("------------------array clear------------");
+
+Array.Clear( dizi, 2, 3);
+
+foreach (var sayi in dizi)
+{
+    Console.WriteLine(sayi);
+}
+
+
+Console.WriteLine("------------------array reverse------------");
+
+Array.Reverse(dizi);
+
+foreach (var sayi in dizi)
+{
+    Console.WriteLine(sayi);
+}
+
+Console.WriteLine("------------------array IndexOf------------");
+
+Console.WriteLine(Array.IndexOf(dizi,6));
+
+Console.WriteLine("------------------array resize------------");
+
+Array.Resize<int>(ref dizi, 9);
+dizi[8] = 89;
+
+foreach (var sayi in dizi)
+{
+    Console.WriteLine(sayi);
+}
+
+
+int a  =  dizi.GetLength(0);
+
+Console.WriteLine("Bu dizi: " + a  + " elemanlıdır.");
+Console.ReadLine();
+
+
+Array.Resize<int>(ref dizi, 3);
+
+foreach (var sayi in dizi)
+{
+    Console.WriteLine(sayi);
+}
